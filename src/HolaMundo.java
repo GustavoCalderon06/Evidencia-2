8ad90324cb38dee62e2d7aacbefb31e130b8399e
j-ortiz-ici <j.ortiz07@ufromail.cl>
2021-09-11 15:51:52 -0300

    commit 4

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,8 @@
+//Joaquín Ortiz Castillo
 public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!!");
+        System.out.println("Joaquín Ortiz Castillo");
     }
-    //as
 }
